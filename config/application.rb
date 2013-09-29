64a74cb48bc81dc359b59e82e92ced44a9892a73
Nefochka <nefirtity@mail.ru>
2013-09-29 22:21:41 +0400

    sixth

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Hamlapp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+    config.assets.precompile += %w(*.png *.jpg *.otf *.svg *.eot *.woff *.ttf)
   end
 end
